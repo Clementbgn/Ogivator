d6034dc439b1b8bc92b5f94f4ba43b056a150a87
Clément Bouguyon <clement.bouguyon@gmail.com>
2024-11-25 21:23:35 +0100

    Ogivator.py addition of the Ressource path

--- src/Ogivator/Ogivator.py
+++ src/Ogivator/Ogivator.py
@@ -3,3 +3,10 @@ import polars as pl
 import webbrowser
 from tkinter import *
 import os
+
+def resource_path(relative_path): #Function to define the relative paths
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath('./src/Ogivator') #Path of the Script and pictures
+    return os.path.join(base_path, relative_path)
